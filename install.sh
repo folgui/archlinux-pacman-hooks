ccc6ce357241ddc639e99a81904912f91489703d
folgui <folguidroid@gmail.com>
2024-07-25 09:15:20 +0200

    added install and uninstall scripts, not neccesary if pkgbuild but there for info

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+
+#DEBUG
+#set -x ## Use “set -x” to enable debug mode
+#set -e ## Use "set -e" to exit on error
+set -u  ## Use "set -u" alway to make sure you won't use any undeclared variable. This saves you from a lot of headaches and critical bugs.
+
+# ============================================================================ #
+# @author       : Copyright (c) 202x Jose Miguel Folgueira <a.k.a. folgui>
+# @email        : folguidroid@gmail.com
+# @web          : http://folgui.synology.me/
+# @github       : https://github.com/folgui
+# @style        : https://gitlab.com/fryntiz/bash-guide-style
+# @license      : https://www.mit.edu/~amini/LICENSE.md
+#
+# Project Name  :
+# Description   :
+# Version       :
+# ============================================================================ #
+
+sudo mkdir -p /etc/pacman.d/hooks
+sudo cp 99-arch-reboot-required.hook /etc/pacman.d/hooks
+sudo cp arch-reboot-required.sh /usr/local/bin
+
+echo "*** arch-reboot-required has been successfully installed ***"
---
+++ uninstall.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+
+#DEBUG
+#set -x ## Use “set -x” to enable debug mode
+#set -e ## Use "set -e" to exit on error
+set -u  ## Use "set -u" alway to make sure you won't use any undeclared variable. This saves you from a lot of headaches and critical bugs.
+
+# ============================================================================ #
+# @author       : Copyright (c) 202x Jose Miguel Folgueira <a.k.a. folgui>
+# @email        : folguidroid@gmail.com
+# @web          : http://folgui.synology.me/
+# @github       : https://github.com/folgui
+# @style        : https://gitlab.com/fryntiz/bash-guide-style
+# @license      : https://www.mit.edu/~amini/LICENSE.md
+#
+# Project Name  :
+# Description   :
+# Version       :
+# ============================================================================ #
+
+sudo rm /etc/pacman.d/hooks/99-arch-reboot-required.hook
+sudo rm /usr/local/bin/arch-reboot-required.sh
+
+echo "*** arch-reboot-required has been successfully uninstalled ***"
