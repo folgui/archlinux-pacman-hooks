f6b78cb3aa40f77ede3e4bfe54a04d358dfa6d08
folgui <folguidroid@gmail.com>
2024-11-30 17:46:25 +0100

    Several structure changes and added new hooks

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #DEBUG
 #set -x ## Use “set -x” to enable debug mode
 #set -e ## Use "set -e" to exit on error
-set -u  ## Use "set -u" alway to make sure you won't use any undeclared variable. This saves you from a lot of headaches and critical bugs.
+set -u ## Use "set -u" alway to make sure you won't use any undeclared variable. This saves you from a lot of headaches and critical bugs.
 
 # ============================================================================ #
 # @author       : Copyright (c) 202x Jose Miguel Folgueira <a.k.a. folgui>
@@ -20,7 +20,8 @@ set -u  ## Use "set -u" alway to make sure you won't use any undeclared variable
 # ============================================================================ #
 
 sudo mkdir -p /etc/pacman.d/hooks
+sudo mkdir -p /etc/pacman.d/hooks.bin
 sudo cp 99-arch-reboot-required.hook /etc/pacman.d/hooks
-sudo cp arch-reboot-required.sh /usr/local/bin
+sudo cp arch-reboot-required.sh /etc/pacman.d/hooks.bin
 
 echo "*** arch-reboot-required has been successfully installed ***"
